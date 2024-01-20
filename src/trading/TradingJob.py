749b78b484b1819bb63f33846f9485bb1896d6cf
Paddy Wardle <paddy.wardle@yahoo.com>
2024-01-20 20:20:12 +0000

    Updating TradingJob for new OpportunityTickers and TechnicalIndicators objects

--- src/trading/TradingJob.py
+++ src/trading/TradingJob.py
@@ -8,6 +8,7 @@ from src.strategies.Strategy import Strategy
 from src.opportunities.Opportunities import Opportunities
 from src.opportunities.OpportunitiesStats import OpportunitiesStats
 from src.opportunities.OpportunitiesURL import OpportunitiesURL
+from src.opportunities.OpportunityTickers import OpportunityTickers
 
 from src.indicators.TechnicalIndicators import TechnicalIndicators
 
@@ -23,26 +24,32 @@ import requests
 class TradingJob:
 
     def __init__(self, buy_job:callable=None, sell_job:callable=None, buy_signal:callable=None, sell_signal:callable=None) -> None:
-        self.strategy = Strategy(buy_signal=buy_signal, sell_signal=sell_signal)
+        
         self.buy_job = buy_job or self.default_buy_job
         self.sell_job = sell_job or self.default_sell_job
+
+        self.strategy = Strategy(buy_signal=buy_signal, sell_signal=sell_signal)
         self.slack_client = SlackClient()
         self.trader = Trader()
+        self.market = Market()
+
         self.trader.authenticate()
 
     def default_buy_job(self, opportunities:Opportunities=None) -> None:
 
-        market = Market()
-
-        if market.is_market_open():
+        if self.market.is_market_open():
             for i, row in opportunities:
                 self.default_buy_it(row)
         else:
-            self.slack_client.send_message(f"{market.market_location} is not open")
+            self.slack_client.send_message(f"{self.market.market_location} is not open")
 
-    def default_buy_it(self, opportunity:pd.Series=None) -> None:
+    def default_buy_it(self, period:str="1y", interval:str="1d", windows:list[str]=None, opportunity:pd.Series=None) -> None:
 
-        op_stats = TechnicalIndicators(symbol=opportunity["Symbol"], period="1y")
+        op = OpportunityTickers(symbol=opportunity["Symbol"], period=period, interval=interval)
+        op_df = op.get_ticker_history()
+
+        indicators = TechnicalIndicators(windows=windows)
+        op_stats = indicators.get_stats(opportunity_tickers=op_df)
         
         sma = op_stats.ticker_history["sma14"].iloc[-1]
         close = op_stats.ticker_history["Close"].iloc[-1]
@@ -60,19 +67,21 @@ class TradingJob:
 
     def default_sell_job(self):
 
-        market = Market()
-        
-        if market.is_market_open():
+        if self.market.is_market_open():
             positions = Positions()
         
             for i, pos in enumerate(positions):
                 self.default_sell_it(pos)
         else:
-            self.slack_client.send_message(f"{market.market_location} is not open")
+            self.slack_client.send_message(f"{self.market.market_location} is not open")
 
-    def default_sell_it(self, position:Position=None) -> None:
+    def default_sell_it(self, period:str="1y", interval:str="1d", windows:list[str]=None, position:Position=None) -> None:
 
-        op_stats = TechnicalIndicators(symbol=position.symbol, period="1y")
+        op = OpportunityTickers(symbol=position.symbol, period=period, interval=interval)
+        op_df = op.get_ticker_history()
+
+        indicators = TechnicalIndicators(windows=windows)
+        op_stats = indicators.get_stats(opportunity_tickers=op_df)
 
         sma = op_stats.ticker_history["sma14"].iloc[-1]
         close = op_stats.ticker_history["Close"].iloc[-1]
